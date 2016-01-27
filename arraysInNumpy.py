a43a0116c46143980fd4b0ea4eb5ac5bfdd04c67
Ashwini Purohit <ashwinipurohit@yahoo.in>
2016-01-27 23:43:54 +0530

    Create arraysInNumpy.py

---
+++ arraysInNumpy.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+my_list1 = [1,2,3,4]
+my_array1
+
+mylist2 = [11,22,33,44]
+
+mylist = [my_list1, mylist2]
+mylist
+myarray = np.array(mylist)
+print myarray
+print myarray.shape
+myarray.dtype
+a = np.zeros(3)
+a.dtype
+np.ones([5,5])
+
+np.empty(4)
+np.eye(4)
